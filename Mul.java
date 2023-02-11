cb083ff681f8b6894c9d31254b35ebe2522501e9
pawan.sharma0702 <pawan.sharma0702@gmail.com>
2023-02-11 10:41:58 +0530

    Mul file added in track

---
+++ Mul.java
@@ -0,0 +1,18 @@
+public class Sum
+{
+public static void main(String [] args)
+{
+int a=10;
+int b=20;
+int d=45;
+int c=a+b;
+int d=a*b;
+int e=a-b;
+
+System.out.println("Sum of A and B"+c);
+System.out.println("Multiplication of A and B"+d);
+System.out.println("Diffrence  of A and B"+e);
+System.out.println("Code is done by Shreya!!"+e);
+System.out.println("Code is done by Ruchi!!"+e);
+}
+}
