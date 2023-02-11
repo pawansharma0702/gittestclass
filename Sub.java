efee71c8615f7f61ede947c98099f854fa64e6bc
pawan.sharma0702 <pawan.sharma0702@gmail.com>
2023-02-11 10:33:08 +0530

    My Sub file is commited

---
+++ Sub.java
@@ -0,0 +1,19 @@
+public class Sum
+{
+public static void main(String [] args)
+{
+int a=10;
+int b=20;
+int d=45;
+int c=a+b;
+int d=a*b;
+int e=a-b;
+
+System.out.println("Sum of A and B"+c);
+System.out.println("Multiplication of A and B"+d);
+System.out.println("Diffrence  of A and B"+e);
+System.out.println("My Test Automation Calss!!");
+System.out.println("Code is done by Yogesh !!");
+
+}
+}
